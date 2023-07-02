ed5a8e5a214c0802d7ddeda7750850e7f3205917
Andres Aguirre <aandres51@hotmail.com>
2023-07-02 12:01:21 -0300

    Se corrigen botones de borrar carrito y mostrar carrito

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 // Almacena el carrito de compras
 let carrito = [];
 
+// Estructura de productos
+class Producto {
+  constructor(id, descripcion, stock, precio) {
+    this.id = id;
+    this.descripcion = descripcion;
+    this.stock = stock;
+    this.precio = precio;
+    this.cantidad = 0;
+  }
+}
+
+// Definicion de los productos a mostrar
+const stockInicial = [5, 8, 10, 3, 4];
+
+const producto1 = new Producto(1, "Vestido 1", stockInicial[0], 1);
+const producto2 = new Producto(2, "Vestido 2", stockInicial[1], 1);
+const producto3 = new Producto(3, "Vestido 3", stockInicial[2], 1);
+const producto4 = new Producto(4, "Vestido 4", stockInicial[3], 1);
+const producto5 = new Producto(5, "Vestido 5", stockInicial[4], 1);
+
+const PRODUCTOS = [producto1, producto2, producto3, producto4, producto5];
+
 // Funcion para mostrar los productos en la pagina
 function renderizarProductos() {
   const cardProductos = document.getElementById("cardProductos");
@@ -50,7 +72,7 @@ function guardarCarritoEnLocalStorage() {
 }
 
 // Funcion para mostrar el resumen del carrito
-function mostrarResumenCarrito() {
+function mostrarResumen() {
   let resumenHTML = "";
   let totalPrecios = 0;
   let totalCantidades = 0;
@@ -100,12 +122,18 @@ function actualizarStock(idProducto, nuevoStock) {
   stockElement.innerText = `Stock: ${nuevoStock}`;
 }
 
-// Funcion para reiniciar el stock de cada producto
-function reiniciarStock() {
+function borrarCarrito() {
+  carrito = []; // Vacía el carrito
   for (const producto of PRODUCTOS) {
-    producto.stock = 5; // Coloca el stock original de cada producto
+    producto.stock = stockInicial[producto.id - 1]; // Coloca el stock original de cada producto
     actualizarStock(producto.id, producto.stock);
   }
+  swal.fire({
+    text: "Carrito vacío",
+    icon: "success"
+  });
+  mostrarResumen(); // Actualiza el resumen del carrito
+  guardarCarritoEnLocalStorage(); // Guarda el carrito en el LocalStorage
 }
 
 // Funcion para agregar un producto al carrito
@@ -122,7 +150,11 @@ function agregarAlCarrito(idProducto) {
     }
 
     if (cantidad > producto.stock) {
-      alert("No hay suficiente stock disponible.");
+      swal.fire({
+        title: "Error",
+        text: "No hay suficiente stock disponible.",
+        icon: "error"
+      });
       return;
     }
 
@@ -144,85 +176,23 @@ function agregarAlCarrito(idProducto) {
     }
 
     // Actualizar el resumen del carrito
-    mostrarResumenCarrito();
-  } else {
-    alert("El producto seleccionado no existe.");
-  }
-  guardarCarritoEnLocalStorage();
-}
+    //mostrarResumen();
+    // Guarda el carrito en el LocalStorage
+    guardarCarritoEnLocalStorage();
 
-// Funcion para borrar el carrito
-function borrarCarrito() {
-  carrito = [];
-  guardarCarritoEnLocalStorage();
-  mostrarResumenCarrito();
-  reiniciarStock(); // Reinicia el stock de cada producto
-}
-
-// Funcion para mostrar el resumen del carrito y ocultar productos
-function mostrarResumen() {
-  const productosCarrito = carrito.filter((producto) => producto.cantidad > 0);
-  const resumenHTML = productosCarrito
-    .map((producto) => {
-      const precioTotalProducto = producto.precio * producto.cantidad;
-      return `
-        <div class="producto-resumen">
-          <span class="nombre-producto">${producto.descripcion}</span>
-          <span class="cantidad-producto">${producto.cantidad}</span>
-          <span class="precio-producto">$${precioTotalProducto.toFixed(2)}</span>
-        </div>
-      `;
-    })
-    .join("");
-
-  const resumenCarritoElement = document.getElementById("resumenCarrito");
-  resumenCarritoElement.innerHTML = resumenHTML;
-
-  // Ocultar productos en el carrito y mostrar resumen
-  const productos = document.querySelectorAll(".card");
-  productos.forEach((producto) => {
-    if (!producto.classList.contains("oculto")) {
-      producto.classList.add("oculto");
-    }
-  });
-
-  const resumen = document.getElementById("resumen");
-  resumen.style.display = "block";
-}
-
-// Funcion para mostrar todos los productos y ocultar el resumen
-function mostrarProductos() {
-  const productos = document.querySelectorAll(".card");
-  productos.forEach((producto) => {
-    if (producto.classList.contains("oculto")) {
-      producto.classList.remove("oculto");
-    }
-  });
-
-  const resumen = document.getElementById("resumen");
-  resumen.style.display = "none";
-}
-
-// Estructura de productos
-class Producto {
-  constructor(id, descripcion, stock, precio, carrito) {
-    this.id = id;
-    this.descripcion = descripcion;
-    this.stock = stock;
-    this.precio = precio;
-    this.cantidad = 0;
+    swal.fire({
+      title: "Producto agregado",
+      text: "El producto ha sido agregado al carrito.",
+      icon: "success"
+    });
   }
 }
 
-// Definicion de los productos a mostrar
-const producto1 = new Producto(1, "Vestido 1", 5, 1);
-const producto2 = new Producto(2, "Vestido 2", 5, 1);
-const producto3 = new Producto(3, "Vestido 3", 5, 1);
-const producto4 = new Producto(4, "Vestido 4", 5, 1);
-const producto5 = new Producto(5, "Vestido 5", 5, 1);
-
-const PRODUCTOS = [producto1, producto2, producto3, producto4, producto5];
-
-renderizarProductos();
+// Carga el carrito almacenado en el Local Storage
 cargarCarritoDesdeLocalStorage();
-mostrarResumenCarrito();
+
+// Muestra los productos en la página
+renderizarProductos();
+
+// Muestra el resumen del carrito
+mostrarResumen();
